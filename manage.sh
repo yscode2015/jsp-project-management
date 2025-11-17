475ff9bbe7bf1b3432826ee2133638db17240a91
YoshifumiMomose <yscode2015@gmail.com>
2025-11-17 17:08:29 +0900

    update

--- manage.sh
+++ manage.sh
@@ -5,11 +5,11 @@ readonly MODE=${1:-build}
 build() {
   source ${ROOT}/config.sh
 
-  bash ${ROOT}/01-vpc/manage.sh build
-
-  if [ ${?} -ne 0 ]; then
-    exit 1
-  fi
+#  bash ${ROOT}/01-vpc/manage.sh build
+#
+#  if [ ${?} -ne 0 ]; then
+#    exit 1
+#  fi
 
   DOMAIN_PREFIX_=${DOMAIN_PREFIX} \
     bash ${ROOT}/02-cognito/manage.sh build
@@ -38,11 +38,11 @@ delete() {
     exit 1
   fi
 
-  bash ${ROOT}/01-vpc/manage.sh delete
-
-  if [ ${?} -ne 0 ]; then
-    exit 1
-  fi
+#  bash ${ROOT}/01-vpc/manage.sh delete
+#
+#  if [ ${?} -ne 0 ]; then
+#    exit 1
+#  fi
 }
 
 if [ ${MODE} = build ]; then
